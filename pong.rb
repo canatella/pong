f64e3928e13ec58f9d3ef175b21590ce6ef05c18
Damien Merenne <damien@bloom-life.com>
2020-01-12 17:17:54 +0100

    Version initiale.

---
+++ pong.rb
@@ -0,0 +1,95 @@
+require 'gosu'
+
+class Pong < Gosu::Window
+  LARGEUR_FENETRE = 640
+  LONGUEUR_FENETRE = 480
+  LARGEUR_JOUEUR = 10
+  LONGUEUR_JOUEUR = 40
+  ESPACE_JOUEUR = 10
+  VITESSE_BALLE = 3
+
+  class Joueur
+    def initialize(x)
+      @longueur = Pong::LONGUEUR_JOUEUR
+      @largeur = Pong::LARGEUR_JOUEUR
+      @x = x
+      @y = (Pong::LONGUEUR_FENETRE - @longueur) / 2
+    end
+
+    def largeur
+      @largeur
+    end
+
+    def x
+      @x
+    end
+
+    def y
+      @y
+    end
+
+    def draw
+      Gosu.draw_rect(self.x, self.y, @largeur, @longueur, Gosu::Color::WHITE)
+    end
+
+    def droite?
+      @x > Pong::LARGEUR_FENETRE / 2
+    end
+  end
+
+  class Balle
+    attr_reader(:x, :y, :diametre, :sens)
+
+    def initialize
+      @diametre = 10
+      @sens = 1
+      @x = (Pong::LARGEUR_FENETRE - @diametre) / 2
+      @y = (Pong::LONGUEUR_FENETRE - @diametre) / 2
+    end
+
+    def draw
+      Gosu.draw_rect(@x, @y, @diametre, @diametre, Gosu::Color::WHITE)
+    end
+
+    def avance
+      @x = @x + @sens * Pong::VITESSE_BALLE
+    end
+
+    def change_sens
+      @sens = 0 - @sens
+    end
+  end
+
+  def initialize
+    super(Pong::LARGEUR_FENETRE, Pong::LONGUEUR_FENETRE)
+    self.caption = "Pong!!!"
+    @joueur1 = Joueur.new(Pong::ESPACE_JOUEUR)
+    @joueur2 = Joueur.new(Pong::LARGEUR_FENETRE - Pong::ESPACE_JOUEUR - Pong::LARGEUR_JOUEUR)
+    @balle = Balle.new
+  end
+
+  def touche_balle?(balle, joueur)
+    if joueur.droite? && balle.x >= joueur.x - (balle.sens * balle.diametre)
+      true
+    elsif !joueur.droite? && balle.x <= joueur.x - (balle.sens * balle.diametre)
+      true
+    else
+      false
+    end
+  end
+
+  def update
+    @balle.avance
+    if touche_balle?(@balle, @joueur2) || touche_balle?(@balle, @joueur1)
+      @balle.change_sens
+    end
+  end
+
+  def draw
+    @joueur1.draw
+    @joueur2.draw
+    @balle.draw
+  end
+end
+
+Pong.new.show
